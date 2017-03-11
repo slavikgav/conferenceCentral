f3787ce61923b2dfe9260440d34982f92cb9ddd1
Yaroslav Gavryshko <gavryshko.sl@gmail.com>
2017-03-11 17:54:21 +0200

    Updated WEB_Client id in Constants

--- src/main/java/com/google/devrel/training/conference/Constants.java
+++ src/main/java/com/google/devrel/training/conference/Constants.java
@@ -6,7 +6,7 @@ import com.google.api.server.spi.Constant;
  * Contains the client IDs and scopes for allowed clients consuming the conference API.
  */
 public class Constants {
-    public static final String WEB_CLIENT_ID = "replace this with your Web cliend ID";
+    public static final String WEB_CLIENT_ID = "830707179256-8u6503sb4nu21ukji4ane4kiojbg3iua.apps.googleusercontent.com";
     public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
     public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
     public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
